9a5c93fcb1f3f3bd1286c4b336cb9d38d58768ef
Nicolas <nicolasdasilvasantos2009@gmail.com>
2026-03-21 22:53:17 -0300

    Resolvendo o Exercício 2

---
+++ Exercicio2.py
@@ -0,0 +1,12 @@
+n1 = int(input("Digite um Número:"))
+n2 = int(input("Digite outro Número:"))
+
+soma= n1+n2
+subtracao= n1-n2
+mult=n1*n2
+divisao=n1/n2
+
+print("Soma:",soma)
+print("Subtracao:",subtracao)
+print("Multiplicacao:",mult)
+print("Divisao:",divisao)
