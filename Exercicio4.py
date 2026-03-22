9074103e03c7b22872ec26ff3f5e68cd9b8bc401
Nicolas <nicolasdasilvasantos2009@gmail.com>
2026-03-22 19:34:19 -0300

    Resolvendo o Exercício 4

---
+++ Exercicio4.py
@@ -0,0 +1,9 @@
+Pcarro = int(input("Qual e o Preço do Carro: R$"))
+
+imposto= Pcarro*0.45
+Lucro= Pcarro*0.28
+Total = Pcarro + Lucro + imposto
+
+print("Preço Inicial Do Carro: R$",Pcarro)
+print("Preço Final do Carro depois dos Impostos: R%",Total)
+print("O lucro do distribuidor foi de: R$",Lucro)
